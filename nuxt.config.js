8b9e4685a7954dfcf78be32246b5abbd45e08d48
vandercoder <andikawilliam@outlook.com>
2021-01-06 16:18:23 +0700

    change fonts to static

--- nuxt.config.js
+++ nuxt.config.js
@@ -16,10 +16,6 @@ export default {
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      {
-        rel: 'stylesheet',
-        href: 'https://fonts.googleapis.com/css2?family=Noto+Sans+JP:wght@100;300;400;500;700;900&display=swap'
-      }
     ]
   },
 
