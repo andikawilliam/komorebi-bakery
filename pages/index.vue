c0576cadbc0931e211f9521bae93306a13f56dbb
vandercoder <andikawilliam@outlook.com>
2021-01-14 16:58:35 +0700

    add image input for home

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,7 @@
   <div
     class="h-screen bg-shadow bg-center bg-cover text-gray-200 relative"
     :style="{
-      'background-image': `url(${require('~/assets/img/komorebi-1.jpg')})`
+      'background-image': `url(${require( home.background )})`
     }"
   >
     <div class="flex justify-center items-center h-screen">
